d51ee3561840d9a760c105f3c0bdb91dc740620a
Sean Stewart <sean.stewart1310@gmail.com>
2018-01-04 21:19:12 +0000

    Intial Work on Mouse Clicks applied to board
    For now this just turns the white boxes blue when clicked, and returns
    back to white when the mouse has stopped hovering over it.

---
+++ Honours Project/Assets/BoxClick.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class BoxClick : MonoBehaviour {
+
+	bool buttonPressed = false;
+	// Use this for initialization
+	public void turnBlue(){
+		if(buttonPressed == false){
+		Debug.Log("Button pressed");
+		GetComponent<Image>().color = Color.cyan;
+		buttonPressed = true; 
+	}
+		// GetComponent<Image>.color = Color.green;
+	}
+
+	public void turnWhite(){
+
+		if(buttonPressed == true){
+			Invoke("changeColour", 2);
+			buttonPressed = false; 
+		}
+		// GetComponent<Image>.color = Color.green;
+	}
+
+	void changeColour(){
+		Debug.Log("Button escpaed");
+		GetComponent<Image>().color = Color.white;
+
+	}
+	void Update () {
+		
+	}
+}
