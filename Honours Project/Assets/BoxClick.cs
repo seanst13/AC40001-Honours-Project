69e7936203ee2f86d4f9997286bee6e4d51d860a
Sean Stewart <sean.stewart1310@gmail.com>
2018-01-04 23:19:21 +0000

    Click now added to the grey blocks.
    Additonally the squares now lose their focus when there is no mouse
    focus for 2 seconds.

--- Honours Project/Assets/BoxClick.cs
+++ Honours Project/Assets/BoxClick.cs
@@ -7,19 +7,29 @@ using UnityEngine.EventSystems;
 public class BoxClick : MonoBehaviour {
 
 	bool buttonPressed = false;
+	bool white = false;
+
+	 Color greybox = new Color(.529f,.529f,.529f);
+
 	// Use this for initialization
 	public void turnBlue(){
-		if(buttonPressed == false){
-		Debug.Log("Button pressed");
-		GetComponent<Image>().color = Color.cyan;
-		buttonPressed = true; 
-	}
-		// GetComponent<Image>.color = Color.green;
+		if(!buttonPressed){
+			Debug.Log("Button pressed");
+			if(GetComponent<Image>().color == Color.white){
+				GetComponent<Image>().color = Color.cyan;
+				white = true; 
+			}else {
+				GetComponent<Image>().color = Color.red;
+				white = false; 
+			}
+		}
+			buttonPressed = true; 
+
 	}
 
 	public void turnWhite(){
 
-		if(buttonPressed == true){
+		if(buttonPressed){
 			Invoke("changeColour", 2);
 			buttonPressed = false; 
 		}
@@ -27,9 +37,13 @@ public class BoxClick : MonoBehaviour {
 	}
 
 	void changeColour(){
+		if (white){
 		Debug.Log("Button escpaed");
 		GetComponent<Image>().color = Color.white;
-
+		} else {
+			Debug.Log("Button escpaed");
+			GetComponent<Image>().color = greybox;
+		}
 	}
 	void Update () {
 		
