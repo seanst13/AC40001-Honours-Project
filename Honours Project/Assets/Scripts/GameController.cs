b7c401d037056dae118022d16bbb05e99b9ea8c3
Sean Stewart <sean.stewart1310@gmail.com>
2018-01-29 20:33:29 +0000

    Refactored Box Generation Code & Started Work on setting middle value.
    Box Generation now only uses one variable for rows and columns so that
    it is consistently a grid.

--- Honours Project/Assets/Scripts/GameController.cs
+++ Honours Project/Assets/Scripts/GameController.cs
@@ -2,16 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour {
-	public int boardRowCount;
-	public int boardColumnCount;
+	public int gridSize; 
 	bool BoxWhite = true; 
 	public GameObject whiteSquare;
 	public GameObject greySquare;
+	private int middleSquare; 
 
 	void Start(){
+		middleSquare = Mathf.RoundToInt(gridSize/2);
 		DisplayBoard(); 
+		
 	}
 
 	public void returnToMenu(){
@@ -21,14 +24,14 @@ public class GameController : MonoBehaviour {
 	public void DisplayBoard(){
 		int xpos = -285;
 		int ypos = 185; 
-		   for (var row = 0; row < this.boardRowCount; row++)
+		   for (var row = 0; row < this.gridSize; row++)
          {
-             for (var column = 0; column < this.boardColumnCount; column++)
+             for (var column = 0; column < this.gridSize; column++)
              {
-				 if (BoxWhite == true){
+				 if (BoxWhite){
                  	Instantiate(whiteSquare, new Vector3(xpos, ypos, 0), Quaternion.identity).transform.SetParent(this.transform,false);
 					BoxWhite = false;
-				 } else if (BoxWhite == false){
+				 } else if (!BoxWhite){
                  	Instantiate(greySquare, new Vector3(xpos, ypos, 0), Quaternion.identity).transform.SetParent(this.transform,false);
 					BoxWhite = true;
              	}
