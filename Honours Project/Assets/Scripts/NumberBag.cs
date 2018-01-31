5932282fb3659a0f308bdc254f90bf9055a31f8c
Sean Stewart <sean.stewart1310@gmail.com>
2018-01-31 22:15:06 +0000

    Added Score fields, Added Number Generation Values
    Generation for the playing pieces is added to a list.
    Next work is to apply these numbers to the playing pieces and choose
    randomly from the list.

---
+++ Honours Project/Assets/Scripts/NumberBag.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberBag : MonoBehaviour {
+	public List<int> numbers;
+	private int number = 1;
+	public int amountToPool = 5;
+
+	// Use this for initialization
+	void Start () {
+// Declare all the numbers that will be used them in the game for the playing pieces and add them to a list. 
+		numbers = new List<int>();
+		for (int i = 0; i < amountToPool; i++) {
+			for (int j = 0; j < amountToPool; j++){
+				numbers.Add(number);
+			}
+			number++; 
+		}
+
+		foreach (int num in numbers){
+		}	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
