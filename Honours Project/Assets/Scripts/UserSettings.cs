d7a97a82150bfc6937906c94093190020bf8eecf
Sean Stewart <sean.stewart1310@gmail.com>
2018-02-06 16:29:12 +0000

    Creation of User Settings Script
    This does nothing at the moment but set and get a playername.

---
+++ Honours Project/Assets/Scripts/UserSettings.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserSettings : MonoBehaviour {
+
+public static string playerName;
+
+void setPlayerName(string name){
+	playerName = name; 
+}
+public string returnPlayerName(){
+	return playerName; 
+	}
+}
