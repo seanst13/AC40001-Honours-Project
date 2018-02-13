2e50af89fc4765160b94f1074ff4115b2c1bba87
Sean Stewart <sean.stewart1310@gmail.com>
2018-02-13 22:42:00 +0000

    Basic Error Management implemented
    This is related to piece placement. When clicking on a square with no
    piece selected it takes two(or more) clicks to display the error.

--- Honours Project/Assets/Scripts/BoxClick.cs
+++ Honours Project/Assets/Scripts/BoxClick.cs
@@ -11,23 +11,33 @@ public class BoxClick : MonoBehaviour {
 	public void boxEnter(){
 		if(!buttonPressed){
 			defaultColour = GetComponent<Image>().color;
-			GetComponent<Image>().color = Color.cyan;
-			string objectname = this.name;
-			Debug.Log(objectname + " has been clicked.");
-
-//Checks if The Piece has been clicked and will place it on the grid if it has. 
+			//Checks if The Piece has been clicked and will place it on the grid if it has. 
 			if (PieceManager.instance.PieceSelected && GetComponentInChildren<Text>().text == ""){
+				GetComponent<Image>().color = Color.cyan;
+				string objectname = this.name;
+				Debug.Log(objectname + " has been clicked.");
+
 				if (PositioningValidation())
 				{
 					GetComponentInChildren<Text>().text = PieceManager.instance.playingPiece.GetComponentInChildren<Text>().text;
 					PieceManager.instance.setPieceValue(); 
 					PieceManager.instance.PieceSelected = false; 
 					GetComponent<Collider2D>().enabled = false; 
+				} else{
+					ErrorManagement.instance.ShowError("Error: Piece must be placed next to an existing piece.");
 				}
+			} else if (PieceManager.instance.PieceSelected && GetComponentInChildren<Text>().text != "") {
+				PieceManager.instance.PieceSelected = false; 
+				ErrorManagement.instance.ShowError("Error: Piece cannot be placed ontop of an existing piece.");
 			}
-
+			
+		} else {
+			PieceManager.instance.PieceSelected = false; 
+			ErrorManagement.instance.ShowError("Error: Please select a piece before placing on the grid.");
+			
 		}
-		buttonPressed = true; 
+		buttonPressed = true; 	
+		
 	}
 	public void boxExit(){
 		if(buttonPressed){
