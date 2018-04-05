b6b9e8b9c606718e0a1395f1cf9ccd022c9fc25d
Sean Stewart <sean.stewart1310@gmail.com>
2018-04-05 19:01:22 +0100

    Added additional Filter Unit Tests

--- Honours Project/Assets/Editor/Tests/FilterTests.cs
+++ Honours Project/Assets/Editor/Tests/FilterTests.cs
@@ -24,7 +24,7 @@ public class FilterTests {
 
 	[Test]
 	public void checkInvalidFilter() {
-		//Checks the invalid filter removes items from the list; 
+		//Checks the invalid filter removes items from the list
 		filters.GetComponent<AI_Player>().GetPossibleMoves();
 		List<Move> moves = filters.GetComponent<AI_Player>().returnPossibleMoves();
 		int beforeFilter = moves.Count;
@@ -35,7 +35,7 @@ public class FilterTests {
 
 	[Test]
 	public void checkCompleteEvenFilterWorks(){
-		//Checks the even filter removes even totaled items in both rows & columns from the list; 
+		//Checks the even filter removes even totaled items in both rows & columns from the list
 		filters.GetComponent<AI_Player>().GetPossibleMoves();
 		List<Move> moves = filters.GetComponent<AI_Player>().returnPossibleMoves();
 		int beforeFilter = moves.Count;
@@ -46,7 +46,7 @@ public class FilterTests {
 
 	[Test]
 	public void checkEvenFilterWorks(){
-		//Checks the even filter removes items from the list; 
+		//Checks the even filter removes items from the list
 		filters.GetComponent<AI_Player>().GetPossibleMoves();
 		List<Move> moves = filters.GetComponent<AI_Player>().returnPossibleMoves();
 		int beforeFilter = moves.Count;
@@ -112,6 +112,57 @@ public class FilterTests {
 		}
 	}
 
+	[Test]
+	public void checkEvenandScore(){
+		//Checks the even and secondary scoring methods can be used in conjunction
+		filters.GetComponent<AI_Player>().GetPossibleMoves();
+		List<Move> moves = filters.GetComponent<AI_Player>().returnPossibleMoves();
+		moves = Filter.removeCompleteEvenTotals(moves);
+		if (moves.Count > 0){
+			List<int> scorebefore = new List<int>();
+			List<int> scoreafter = new List<int>();
+			foreach(Move m in moves){
+				scorebefore.Add(m.totalScore);
+			} 
+			moves = Filter.addSecondaryScoring(moves);
+			foreach(Move m in moves){
+				scoreafter.Add(m.totalScore);
+			}
+
+			for(int i = 0; i < moves.Count; i ++){
+				Assert.GreaterOrEqual(scoreafter[i], scorebefore[i]);
+			}
+		} else {
+			Assert.IsEmpty(moves); 
+		}
+	}
+
+	[Test]
+	public void checkInvalidEvenandScore(){
+		//Checks if the invalid, even and secondary scoring methods can be used in conjunction 
+		filters.GetComponent<AI_Player>().GetPossibleMoves();
+		List<Move> moves = filters.GetComponent<AI_Player>().returnPossibleMoves();
+		moves = Filter.removeInValidPlacements(moves);
+		moves = Filter.removeCompleteEvenTotals(moves);
+		if (moves.Count > 0){
+			List<int> scorebefore = new List<int>();
+			List<int> scoreafter = new List<int>();
+			foreach(Move m in moves){
+				scorebefore.Add(m.totalScore);
+			} 
+			moves = Filter.addSecondaryScoring(moves);
+			foreach(Move m in moves){
+				scoreafter.Add(m.totalScore);
+			}
+
+			for(int i = 0; i < moves.Count; i ++){
+				Assert.GreaterOrEqual(scoreafter[i], scorebefore[i]);
+			}
+		} else {
+			Assert.IsEmpty(moves); 
+		}
+	}
+	
 	[TearDown]
 	public void TearDown(){
 		GameObject.DestroyImmediate(filters); 
